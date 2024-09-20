746a30c195296bd19faab8b01b179efda7b03424
NetaliRA <natalianak4@gmail.com>
2024-09-21 02:24:34 +0300

    Add files via upload

--- воодный испраленный.py
+++ воодный испраленный.py
@@ -1,4 +1,4 @@
-print(9**0.5*5)  #1st program
-print(9.99>9.98 and 1000!=1000.1)    #2st program
-print(2*2+2==2*(2+2))   #3st program
-print(int(float("123.456")*10)%10)    #4st program
+print(9 ** 0.5 * 5)  #1st program
+print(9.99 > 9.98 and 1000 != 1000.1)    #2st program
+print(2 * 2 + 2 == 2 * (2 + 2))   #3st program
+print(int(float("123.456") * 10) % 10)    #4st program
